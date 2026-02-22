d45b24f298a74dc8b9bffeac30c3cdf3e1d36b63
Big Keith <>
2026-02-22 12:41:40 +0100

    feat: add absoluteUrl helper to utils (review step 4.1)

--- apps/www/lib/utils.ts
+++ apps/www/lib/utils.ts
@@ -1,6 +1,12 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+import { siteConfig } from "@/lib/config"
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function absoluteUrl(path: string) {
+  return `${siteConfig.url}${path}`
 }
